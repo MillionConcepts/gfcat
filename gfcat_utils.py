5ceafd5cc30a7ef83c50d6b2a7070030b8a74742
Chase Million <1483210+cmillion@users.noreply.github.com>
2022-09-24 11:43:57 -0400

    removing safety net

--- gfcat_utils.py
+++ gfcat_utils.py
@@ -338,7 +338,7 @@ def screen_gfcat(eclipses:list,band='NUV',aper_radius=12.8,photdir='/Users/cm/GF
         if not os.path.exists(photpath):
             os.makedirs(f"{photdir}/{edir}/")
             cmd = f"aws s3 sync s3://dream-pool/{edir}/ {photdir}/{edir}/. --exclude '*{'f' if band == 'NUV' else 'n'}d*' --exclude '*raw6*' --exclude '*fits*'"
-            os.system(command)
+            os.system(cmd)
         if not os.path.exists(photpath):
             os.system(f"rm -rf {photdir}/{edir}/")
             continue # there is no photometry file for this eclipse + band
